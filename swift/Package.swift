9c2cd4c5e7bcf29d91c13821378a4c82d4e28da4
Attila-Fejer <attila@fejer.io>
2025-04-08 19:49:58 +0200

    feat: add swift version

---
+++ swift/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "MarsRover",
+    products: [
+        .library(
+            name: "MarsRover",
+            targets: ["MarsRover"]),
+    ],
+    targets: [
+        .target(
+            name: "MarsRover",
+            dependencies: [],
+            path: "Sources/MarsRover"),
+        .testTarget(
+            name: "MarsRoverTests",
+            dependencies: ["MarsRover"],
+            path: "Tests/MarsRoverTests"),
+    ]
+)
